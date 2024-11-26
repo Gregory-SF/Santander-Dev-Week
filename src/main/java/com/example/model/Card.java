0b4a0b20c5a377ab599677a21f13cd1959f927a6
Gregory Soares <gregory16704@gmail.com>
2024-11-26 17:39:23 -0300

    Mudanças

---
+++ src/main/java/com/example/model/Card.java
@@ -0,0 +1,42 @@
+package com.example.model;
+
+import jakarta.persistence.*;
+
+@Entity(name = "card")
+public class Card {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String number;
+
+    @Column(name = "available_limit", scale = 13, precision = 2)
+    private String limit;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getLimit() {
+        return limit;
+    }
+
+    public void setLimit(String limit) {
+        this.limit = limit;
+    }
+
+}
