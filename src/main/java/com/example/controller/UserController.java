6d6fea97deb1dbcfc1f5e93c18fd7765d35f1ef1
Gregory Soares <gregory16704@gmail.com>
2024-12-08 23:24:08 -0300

    Finalizando, mas está com problema de conexão com o railway

---
+++ src/main/java/com/example/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.example.controller;
+
+import com.example.domain.model.User;
+import com.example.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    // É uma boa prática criar um mode de response assim como tinha no código do Helon, pois haverá situações na qual você não irá precisar de todas as informações do modelo, então você cria uma classe específica para a response com apenas as informações necessárias
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        var user = userService.findById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User userToCreate){
+        var userCreated = userService.create(userToCreate);
+        URI location  = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(userCreated.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(userCreated);
+    }
+
+
+}
